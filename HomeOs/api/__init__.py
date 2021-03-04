c6d939a11ee8707373776641316a255760a5facd
donkere-vader <donkere.v@gmail.com>
2021-03-04 17:57:59 +0100

    Updated the database structure and upated TODO

--- HomeOs/api/__init__.py
+++ HomeOs/api/__init__.py
@@ -3,7 +3,6 @@ from picodb import PicoDb
 db = PicoDb("database.json", indent=4, structure={
     "user": {},
     "device": {},
-    "program": {},
     "event": {},
-    "auth_keys": {},
+    "api_key": {},
 })
